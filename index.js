ccc136efec53c6253fe22a2624581882846d87f2
eunthebear <eun.david@gmail.com>
2017-11-13 04:44:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function destructivelyAppendDriver(name) {
   drivers.push(name);
   return drivers;
 }
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+  return drivers;
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name);
+  return drivers;
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name);
+  return drivers;
+}
+
